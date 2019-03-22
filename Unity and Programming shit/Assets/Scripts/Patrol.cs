fac330ab8e98f824e98844e630bf751fc8dbd601
Rattenjunge <les.dams96@gmail.com>
2019-03-22 19:25:16 +0100

    please send help

---
+++ Unity and Programming shit/Assets/Scripts/Patrol.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] points;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float seeDistance = 20f;
+    [SerializeField]
+    private FOVEnemy fovEnemy;
+    [SerializeField]
+    private float minSpeed = 1.5f;
+    [SerializeField]
+    private float maxSpeed = 2f;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    private Vector3 lastPlayerPosition;
+    private Vector3 resetPosition = new Vector3(-100, -100, -100);
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+
+    }
+
+
+    void GotoNextPoint()
+    {
+
+        if (points.Length == 0)
+            return;
+
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * seeDistance, Color.red);
+        if (fovEnemy.visibleTargets.Count == 0)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                if (lastPlayerPosition != resetPosition)
+                {
+                    lastPlayerPosition = resetPosition;
+                }
+                if (agent.speed != minSpeed)
+                {
+                    agent.speed = minSpeed;
+                }
+
+                GotoNextPoint();
+            }
+        }
+        else
+        {
+            lastPlayerPosition = player.transform.position;
+            agent.destination = lastPlayerPosition;
+            if(agent.speed != maxSpeed)
+            {
+                agent.speed = maxSpeed; 
+            }
+            Debug.Log("Found player");
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
---
+++ Unity and Programming shit/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rbody;
+    Camera viewCamera;
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        transform.LookAt(mousePos + Vector3.up * transform.position.y);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal")*2, 0, Input.GetAxisRaw("Vertical")*2);
+
+    }
+    private void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
